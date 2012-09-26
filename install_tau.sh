1c9c8ea5dddb7e37e9c8a9bbaaeca33dd6d0e915
Ethan Schoonover <es@ethanschoonover.com>
2012-09-26 15:04:10 -0700

    updated install sample to point to master branch

--- install_tau.sh
+++ install_tau.sh
@@ -15,7 +15,7 @@
 # curl -sfL http://git.io/rQx7Xw > install.sh; sh install.sh
 
 # CONFIG -----------------------------------------------------------------
-REMOTE=https://raw.github.com/altercation/archblocks/dev
+REMOTE=https://raw.github.com/altercation/archblocks/master
 HOSTNAME=tau
 SYSTEMTYPE=thinkpad_x220
 USERNAME=es
