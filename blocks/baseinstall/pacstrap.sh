e62aeb8be843f7e97e0da8f8c5fa01247194f3c8
Ethan Schoonover <es@ethanschoonover.com>
2012-09-29 01:16:49 -0700

    fixed filesystem sequence

--- blocks/baseinstall/pacstrap.sh
+++ blocks/baseinstall/pacstrap.sh
@@ -3,16 +3,16 @@
 # ------------------------------------------------------------------------
 
 # makes filesystem (provided by FILESYSTEM block)
-FILESYSTEM_PRE_BASEINSTALL
+_filesystem_pre_baseinstall
 
 # standard pacstrap helper script
 pacstrap ${MOUNT_PATH} base base-devel
 
 # write filesystem configs (provided by FILESYSTEM block)
-FILESYSTEM_POST_BASEINSTALL
+_filesystem_post_baseinstall
 
 # unmount efi boot part (provided by FILESYSTEM block)
-FILESYSTEM_PRE_CHROOT
+_filesystem_pre_chroot
 
 # arch-chroot and proceed with script
 _chroot_postscript
