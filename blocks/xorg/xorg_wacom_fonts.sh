b0026150be974016169e3722886c92930829b84e
Ethan Schoonover <es@ethanschoonover.com>
2012-10-01 18:46:45 -0700

    updated blocks

--- blocks/xorg/xorg_wacom_fonts.sh
+++ blocks/xorg/xorg_wacom_fonts.sh
@@ -13,7 +13,10 @@ _installaur webcore-fonts
 _installaur ttf-google-webfonts
 _installaur libspiro
 _installaur fontforge
-_installaur fontconfig-infinality-git
+
+#working, but blowing past memory limit
+#_installaur fontconfig-infinality-git
+
 #packer -S freetype2-git-infinality # will prompt for freetype2 replacement
 # TODO: sed infinality and change to OSX or OSX2 mode
 #	and create the sym link from /etc/fonts/conf.avail to conf.d
