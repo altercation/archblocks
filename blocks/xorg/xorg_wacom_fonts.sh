f1993f11aa78b600a1836fc37599c9cc9c961a58
Ethan Schoonover <es@ethanschoonover.com>
2012-10-01 18:05:26 -0700

    added xmonad block

--- blocks/xorg/xorg_wacom_fonts.sh
+++ blocks/xorg/xorg_wacom_fonts.sh
@@ -12,7 +12,8 @@ _installpkg terminus-font
 _installaur webcore-fonts
 _installaur libspiro
 _installaur fontforge
-packer -S freetype2-git-infinality # will prompt for freetype2 replacement
+_installaur fontconfig-infinality-git
+#packer -S freetype2-git-infinality # will prompt for freetype2 replacement
 # TODO: sed infinality and change to OSX or OSX2 mode
 #	and create the sym link from /etc/fonts/conf.avail to conf.d
 
