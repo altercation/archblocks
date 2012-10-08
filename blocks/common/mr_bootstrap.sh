e2ac48ef0c6d2d7c0dce4dc066b2fcdc94a1f401
Ethan Schoonover <es@ethanschoonover.com>
2012-10-07 20:38:25 -0700

    new repeat until success helper

--- blocks/common/mr_bootstrap.sh
+++ blocks/common/mr_bootstrap.sh
@@ -3,4 +3,18 @@
 # su new user
 # ------------------------------------------------------------------------
 _installaur mr
+
+echo "Do you want to authenticate with a username/passphrase?"
+echo "This will be sent in the following format:"
+echo "https://username:passphrase@rest.of.url.here"
+echo -e "\n(y/n)"
+read _answer
+[ "$_answer" == y* ]
+
+_double_check_until_match "Repository username:"
+_auth_username="$_DOUBLE_CHECK_RESULT"
+
+_double_check_until_match "Github passphrase for username $_github_username"
+_auth_passphrase="$_DOUBLE_CHECK_RESULT"
+
 su $USERNAME -l -c "mr --trust-all bootstrap \"${MR_BOOTSTRAP}\""
