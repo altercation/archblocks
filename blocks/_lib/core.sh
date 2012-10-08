bb64bbb6f2fa02d9a01e6c38ee874849944ec30a
Ethan Schoonover <es@ethanschoonover.com>
2012-10-08 13:38:06 -0700

    fixed /tmp remount

--- blocks/_lib/core.sh
+++ blocks/_lib/core.sh
@@ -84,7 +84,7 @@ fi
 
 # ARCH CONFIG (POST CHROOT) ----------------------------------------------
 if $INCHROOT; then
-umount /tmp; mount -t tmpfs tmp "$1/tmp" -o mode=1777,strictatime,nodev,nosuid,size=150M
+umount /tmp && mount -t tmpfs tmp "/tmp" -o mode=1777,strictatime,nodev,nosuid,size=150M
 _loadblock "${FILESYSTEM}"      # LOAD FILESYSTEM FUNCTIONS
 pacman -Sy
 _filesystem_post_chroot         # FILESYSTEM POST-CHROOT CONFIGURATION
