1baeb9707ef90924770538df47f4d39ec4a990c1
Ethan Schoonover <es@ethanschoonover.com>
2012-10-07 18:32:02 -0700

    added failure check on cryptsetup

--- blocks/filesystem/gpt_luks_passphrase_ext4.sh
+++ blocks/filesystem/gpt_luks_passphrase_ext4.sh
@@ -47,14 +47,14 @@ sgdisk -c ${PARTITION_CRYPT_ROOT}:"${LABEL_ROOT}" ${DRIVE}
 # format LUKS on root
 
 # let cryptsetup handle password entry, exit after 3 successive failures
-_tries=0; _failed=true; while _failed; do
+_tries=0; _failed=true; while $_failed; do
 _tries=$((_tries+1))
 cryptsetup --cipher=aes-xts-plain --verify-passphrase --key-size=512 luksFormat ${DRIVE}${PARTITION_CRYPT_ROOT}
 [ $_tries -gt 2 ] && exit;
 [ $? -eq 0 ] && _failed=false; done
 
 # let cryptsetup handle password entry, exit after 3 successive failures
-_tries=0; _failed=true; while _failed; do
+_tries=0; _failed=true; while $_failed; do
 _tries=$((_tries+1))
 cryptsetup luksOpen ${DRIVE}${PARTITION_CRYPT_ROOT} ${LABEL_ROOT_CRYPT}
 [ $_tries -gt 2 ] && exit;
