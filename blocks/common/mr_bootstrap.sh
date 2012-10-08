c7a51a083e86f1be132b6be0aa365c584b5605dd
Ethan Schoonover <es@ethanschoonover.com>
2012-10-07 22:12:38 -0700

    updated mr bootstrap block to auth

--- blocks/common/mr_bootstrap.sh
+++ blocks/common/mr_bootstrap.sh
@@ -15,15 +15,13 @@ read _answer
 shopt -s nocasematch; [[ "$_answer" == y* ]] && _auth=true; shopt -u nocasematch
 
 if $_auth; then
-_double_check_until_match "Repository username:"
-_auth_username="$_DOUBLE_CHECK_RESULT"
+read -p "Repository username:" _auth_username
 
 _double_check_until_match "Github passphrase for username $_github_username"
 _auth_passphrase="$_DOUBLE_CHECK_RESULT"
 
 MR_BOOTSTRAP="$(echo "${MR_BOOTSTRAP}" | sed "s+^\(.*://\)\(.*\)$+\1${_auth_username}:${_auth_password}@\2+")"
-
 fi
 
-su $USERNAME -l -c "cat "AUTH_USERPASS=$_auth_username $_auth_passphrase" > test.txt; export AUTH_USERNAME=\"$_auth_username\"; export AUTH_PASSPHRASE=\"$_auth_passphrase\"; mr --trust-all bootstrap \"${MR_BOOTSTRAP}\""
+su $USERNAME -l -c "export AUTH_USERNAME=\"$_auth_username\"; export AUTH_PASSPHRASE=\"$_auth_passphrase\"; mr --trust-all bootstrap \"${MR_BOOTSTRAP}\""
 
