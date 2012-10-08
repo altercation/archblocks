99429bc94ac30e1e6810d15cb031914f0a0d4383
Ethan Schoonover <es@ethanschoonover.com>
2012-10-07 22:28:43 -0700

    removed broken mr bootstrap auth

--- blocks/common/mr_bootstrap.sh
+++ blocks/common/mr_bootstrap.sh
@@ -4,24 +4,24 @@
 # ------------------------------------------------------------------------
 _installaur mr
 
-_auth=false
-echo -e "\nYou specified the following 'mr' bootstrap url:"
-echo -e "$MR_BOOTSTRAP\n"
-echo -e "\nDo you want to authenticate with a username/passphrase?"
-echo "This will be submitted in the following format:"
-echo "https://username:passphrase@rest.of.url.here"
-echo -e "\n(y/n)"
-read _answer
-shopt -s nocasematch; [[ "$_answer" == y* ]] && _auth=true; shopt -u nocasematch
-
-if $_auth; then
-read -p "Repository username:" _auth_username
-
-_double_check_until_match "Github passphrase for username $_github_username"
-_auth_passphrase="$_DOUBLE_CHECK_RESULT"
-
-MR_BOOTSTRAP="$(echo "${MR_BOOTSTRAP}" | sed "s+^\(.*://\)\(.*\)$+\1${_auth_username}:${_auth_password}@\2+")"
-fi
+#_auth=false
+#echo -e "\nYou specified the following 'mr' bootstrap url:"
+#echo -e "$MR_BOOTSTRAP\n"
+#echo -e "\nDo you want to authenticate with a username/passphrase?"
+#echo "This will be submitted in the following format:"
+#echo "https://username:passphrase@rest.of.url.here"
+#echo -e "\n(y/n)"
+#read _answer
+#shopt -s nocasematch; [[ "$_answer" == y* ]] && _auth=true; shopt -u nocasematch
+#
+#if $_auth; then
+#read -p "Repository username:" _auth_username
+#
+#_double_check_until_match "Github passphrase for username $_github_username"
+#_auth_passphrase="$_DOUBLE_CHECK_RESULT"
+#
+#MR_BOOTSTRAP="$(echo "${MR_BOOTSTRAP}" | sed "s+^\(.*://\)\(.*\)$+\1${_auth_username}:${_auth_passphrase}@\2+")"
+#fi
 
 su $USERNAME -l -c "export AUTH_USERNAME=\"$_auth_username\"; export AUTH_PASSPHRASE=\"$_auth_passphrase\"; mr --trust-all bootstrap \"${MR_BOOTSTRAP}\""
 
