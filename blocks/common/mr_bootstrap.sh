bb4ba76c17ca354529a02529839f4774fd291c76
Ethan Schoonover <es@ethanschoonover.com>
2012-10-07 21:06:11 -0700

    new mr bootstrap block

--- blocks/common/mr_bootstrap.sh
+++ blocks/common/mr_bootstrap.sh
@@ -4,17 +4,28 @@
 # ------------------------------------------------------------------------
 _installaur mr
 
-echo "Do you want to authenticate with a username/passphrase?"
-echo "This will be sent in the following format:"
+_auth=false
+echo -e "\nYou specified the following 'mr' bootstrap url:"
+echo -e "$MR_BOOTSTRAP\n"
+echo -e "\nDo you want to authenticate with a username/passphrase?"
+echo "This will be submitted in the following format:"
 echo "https://username:passphrase@rest.of.url.here"
 echo -e "\n(y/n)"
 read _answer
-[ "$_answer" == y* ]
+shopt -s nocasematch; [[ "$_answer" == y* ]] && _auth=true; shopt -u nocasematch
 
+if $_auth; then
 _double_check_until_match "Repository username:"
 _auth_username="$_DOUBLE_CHECK_RESULT"
 
 _double_check_until_match "Github passphrase for username $_github_username"
 _auth_passphrase="$_DOUBLE_CHECK_RESULT"
+
+MR_BOOTSTRAP="$(echo "${MR_BOOTSTRAP}" | sed "s+^\(.*://\)\(.*\)$+\1${_auth_username}:${_auth_password}@\2+")"
+
+# we're exporting env with su below, so these variables should be accessible to the mr config file as well
+export AUTH_USERNAME="$_auth_username"
+export AUTH_PASSWORD="$_auth_password"
+fi
 
 su $USERNAME -l -c "mr --trust-all bootstrap \"${MR_BOOTSTRAP}\""
