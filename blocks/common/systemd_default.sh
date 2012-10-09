beba67586986fa708a1d26d486cb2e440d909d38
Ethan Schoonover <es@ethanschoonover.com>
2012-10-09 11:41:57 -0700

    prep for pure systemd install

--- blocks/common/systemd_default.sh
+++ blocks/common/systemd_default.sh
@@ -3,9 +3,11 @@
 # INIT - systemd, pure
 # as per the Arch Linux wiki page on systemd: https://wiki.archlinux.org/index.php/Systemd
 
+_anykey ">>>>>>>>>>>SYSTEMD TEST"
+
 _installpkg systemd
 pacman -S systemd-sysvcompat # interactive
-pacman -R initscripts # going for pure systemd
+pacman -R --noconfirm initscripts # going for pure systemd
 
 # NOT NEEDED IN PURE SYSTEMD MODE
 # KERNEL_PARAMS="${KERNEL_PARAMS:+${KERNEL_PARAMS} }init=/bin/systemd"
