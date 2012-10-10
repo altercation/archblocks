75bf98d8213d57305299bb38d770869550950b91
Ethan Schoonover <es@ethanschoonover.com>
2012-10-09 19:24:26 -0700

    added chrony conf files

--- blocks/common/time_chrony_utc.sh
+++ blocks/common/time_chrony_utc.sh
@@ -4,27 +4,45 @@
 
 ln -sf /usr/share/zoneinfo/${TIMEZONE} /etc/localtime
 echo ${TIMEZONE} >> /etc/timezone
-hwclock --systohc --utc # set hardware clock
+#hwclock --systohc --utc # set hardware clock
+hwclock -w --utc # set hardware clock
 _installpkg chrony
 
 if _systemd; then
-    systemctl enable chrony.service
+    systemctl enable chrony.service;
 else
-    sed -i "/^DAEMONS/ s/hwclock /!hwclock chrony /" /etc/rc.conf
+    sed -i "/^DAEMONS/ s/hwclock /!hwclock chrony /" /etc/rc.conf;
 fi
 
-#cat >> /etc/chrony.conf << EOF
-#
-#server 0.pool.ntp.org offline
-#server 1.pool.ntp.org offline
-#server 2.pool.ntp.org offline
-#server 3.pool.ntp.org offline
-#EOF
+mv /etc/chrony.conf /etc/chrony.conf.orig
 
-cat >> /etc/chrony.conf << EOF
-
+cat > /etc/chrony.conf << EOF
+driftfile /etc/chrony.drift
+keyfile /etc/chrony.keys
+commandkey 1
+dumponexit
+dumpdir /var/log/chrony
+pidfile /var/run/chronyd.pid
+cmdallow 127.0.0.1
+rtcfile /etc/chrony.rtc
+rtconutc
 server 0.pool.ntp.org offline
 server 1.pool.ntp.org offline
 server 2.pool.ntp.org offline
 server 3.pool.ntp.org offline
 EOF
+
+echo "1 mypassword" > /etc/chrony.keys
+
+systemctl start chrony
+
+_anykey "TESTING CHRONY >>>>>>"
+# chronyc makestep to adjust if large differential
+chronyc << EOF
+password mypassword
+online
+makestep
+trimrtc
+writertc
+EOF
+_anykey "TESTING CHRONY >>>>>>"
--- blocks/network/wired_wireless_default.sh
+++ blocks/network/wired_wireless_default.sh
@@ -17,7 +17,7 @@ _installpkg wpa_supplicant # (optional) - for wireless networking support
 if _systemd; then
     systemctl enable net-auto-wired.service
     systemctl enable net-auto-wireless.service
-    systemctl enable netcfg.service
+    # systemctl enable netcfg.service # - uses profiles listed in /etc/conf.d/netcfg NETWORKS=(...)
     # systemctl enable netcfg@PROFILENAME.service
 else
     _daemon_remove network
