2a90b0123a743653c5f29cf41db462d370697241
Ethan Schoonover <es@ethanschoonover.com>
2012-10-09 19:51:57 -0700

    updated sample laptop installer with systemd note, chrony

--- sample_laptop.sh
+++ sample_laptop.sh
@@ -25,7 +25,8 @@ KEYMAP=us
 TIMEZONE=US/Pacific
 MODULES="dm_mod dm_crypt aes_x86_64 ext2 ext4 vfat intel_agp drm i915"
 HOOKS="base udev autodetect pata scsi sata usb usbinput consolefont encrypt filesystems fsck"
-#KERNEL_PARAMS="quiet" # set/used in FILESYSTEM,INIT,BOOTLOADER blocks (this gets added to)
+INIT_MODE=systemd # systemd is the default value; change to anything else or blank to skip systemd install/config
+KERNEL_PARAMS="quiet" # set/used in FILESYSTEM,INIT,BOOTLOADER blocks (this gets added to)
 INSTALL_DRIVE=query # "/dev/sda" "query" or blank (blank is the same as "query")
 
 # DOTFILES / HOME SETUP --------------------------------------------------
